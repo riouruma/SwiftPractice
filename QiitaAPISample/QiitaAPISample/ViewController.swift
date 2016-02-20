1d98e171fca434245db5389449b30ee4bad20a5f
yokota <macneko.ayu@gmail.com>
2016-02-20 16:53:36 +0900

    QiitaAPISampleプロジェクト追加

---
+++ QiitaAPISample/QiitaAPISample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QiitaAPISample
+//
+//  Created by macneko_ayu on 2016/02/20.
+//  Copyright © 2016年 macneko.leno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
